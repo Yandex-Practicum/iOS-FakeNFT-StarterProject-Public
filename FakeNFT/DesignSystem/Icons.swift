adf72b9e9c151f4ecb2a02330d8d4238526a2a21
Andrey Sysoev <andrey@sysoev.ru>
2023-06-17 21:23:15 +0300

    Добавлены иконки

---
+++ FakeNFT/DesignSystem/Icons.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIImage {
+    enum Icons {
+        static let back = UIImage(named: "Back")
+        static let close = UIImage(named: "Close")
+        static let sort = UIImage(named: "Sort")
+    }
+}
