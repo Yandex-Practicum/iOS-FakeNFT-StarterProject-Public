9baa3f59b9c000f580c170f07b4cf3991e77cd5c
Ilya Valito <valito.spb@gmail.com>
2023-06-19 20:29:44 +0300

    Created a ProfileScreenController with a Profile Screen UI

---
+++ FakeNFT/Scenes /ProfileScene/ProfileScreen/View/ProfileScreenController.swift
@@ -0,0 +1,150 @@
+//
+//  ProfileScreenController.swift
+//  FakeNFT
+//
+//  Created by Илья Валито on 19.06.2023.
+//
+
+import UIKit
+
+// MARK: - ProfileScreenController
+final class ProfileScreenController: UIViewController {
+
+    // MARK: - Properties and Initializers
+    private var viewModel: ProfileScreenViewModel?
+
+    private let noInternetLabel = {
+       let label = UICreator.shared.makeLabel(text: "Нет интернета", font: UIFont.appFont(.bold, withSize: 17))
+        label.isHidden = true
+        return label
+    }()
+    private let profileImageView = UICreator.shared.makeImageView(cornerRadius: 35)
+    private let profileNameLabel = UICreator.shared.makeLabel()
+    private let profileDescriptionLabel = UICreator.shared.makeLabel(font: UIFont.appFont(.regular, withSize: 13))
+    private let profileLinkTextView = UICreator.shared.makeLinkTextView()
+    private let profileMenuTableView = {
+        let tableView = UICreator.shared.makeTableView(isScrollable: false)
+        tableView.register(ProfileMenuCell.self,
+                           forCellReuseIdentifier: Constants.CollectionElementNames.profileMenuCell)
+        tableView.showsVerticalScrollIndicator = false
+        return tableView
+    }()
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        view.backgroundColor = .appWhite
+        setupAutolayout()
+        addSubviews()
+        setupConstraints()
+        profileMenuTableView.dataSource = self
+        profileMenuTableView.delegate = self
+        showOrHideUI()
+        viewModel = ProfileScreenViewModel()
+        bind()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel?.checkForData()
+    }
+}
+
+// MARK: - Helpers
+extension ProfileScreenController {
+
+    private func setupAutolayout() {
+        noInternetLabel.toAutolayout()
+        profileImageView.toAutolayout()
+        profileNameLabel.toAutolayout()
+        profileDescriptionLabel.toAutolayout()
+        profileLinkTextView.toAutolayout()
+        profileMenuTableView.toAutolayout()
+    }
+
+    private func addSubviews() {
+        view.addSubview(noInternetLabel)
+        view.addSubview(profileImageView)
+        view.addSubview(profileNameLabel)
+        view.addSubview(profileDescriptionLabel)
+        view.addSubview(profileLinkTextView)
+        view.addSubview(profileMenuTableView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            noInternetLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            noInternetLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.heightAnchor.constraint(equalTo: profileImageView.widthAnchor),
+            profileNameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16),
+            profileNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            profileNameLabel.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            profileDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            profileDescriptionLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 20),
+            profileDescriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18),
+            profileLinkTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            profileLinkTextView.topAnchor.constraint(equalTo: profileDescriptionLabel.bottomAnchor, constant: 12),
+            profileLinkTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18),
+            profileLinkTextView.heightAnchor.constraint(equalToConstant: 20),
+            profileMenuTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileMenuTableView.topAnchor.constraint(equalTo: profileLinkTextView.bottomAnchor, constant: 44),
+            profileMenuTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileMenuTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    private func bind() {
+        guard let viewModel = viewModel else { return }
+        viewModel.$canShowUI.bind { [weak self] newValue in
+            guard let self else { return }
+            if newValue {
+                self.fillUI()
+                self.showOrHideUI()
+            }
+        }
+    }
+
+    private func showOrHideUI() {
+        noInternetLabel.isHidden.toggle()
+        profileImageView.isHidden.toggle()
+        profileNameLabel.isHidden.toggle()
+        profileDescriptionLabel.isHidden.toggle()
+        profileLinkTextView.isHidden.toggle()
+        profileMenuTableView.isHidden.toggle()
+    }
+
+    private func fillUI() {
+        guard let viewModel else { return }
+        profileImageView.image = UIImage(named: viewModel.giveData())
+        profileNameLabel.text = viewModel.giveData()
+        profileDescriptionLabel.text = """
+Here we stand, worlds apart, hearths broken in two, two, two...
+Slepless night, losing ground, i'm reaching for you, you, you...
+Feeling that it's gone can change your mind.
+If we can't go on to survive the tide, love divide.
+"""
+        profileLinkTextView.text = "www.google.com"
+    }
+}
+
+// MARK: - UITableViewDataSourceDelegate
+extension ProfileScreenController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.giveNumberOfMenuCells() ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        viewModel?.configureCell(forTableView: tableView, atRow: indexPath.row) ?? UITableViewCell()
+    }
+}
+
+// MARK: - UITableViewDataSourceDelegate
+extension ProfileScreenController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
