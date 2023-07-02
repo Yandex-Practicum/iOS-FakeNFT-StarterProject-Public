4ae6cdd3d32fd6c1f2ce8b1b4fce1b0e8c5c3328
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-07-02 10:31:57 +0300

    tabBar refactoring

---
+++ FakeNFT/Scenes /Profile/ProfileViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
