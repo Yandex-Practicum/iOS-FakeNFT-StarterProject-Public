992dfc68f530511a266a1678494c1f3846dc08aa
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-08-02 16:09:05 +0700

    Добавление всех изображений

--- FakeNFT/DesignSystem/UIImage+Extensions.swift
+++ FakeNFT/DesignSystem/UIImage+Extensions.swift
@@ -13,8 +13,36 @@ extension UIImage {
         static let catalog = UIImage.named(AppConstants.Icons.catalog)
         static let profile = UIImage.named(AppConstants.Icons.profile)
         static let statistics = UIImage.named(AppConstants.Icons.statistics)
+        static let close = UIImage.named("Close")
+        static let sort = UIImage.named("Sort")
         
         static let star = UIImage(systemName: "star.fill")
+    }
+
+    enum Cart {
+        static let active = UIImage.named("CartActive")
+        static let nonActive = UIImage.named("CartNonActive")
+    }
+
+    enum Add {
+        static let active = UIImage.named("AddActive")
+        static let done = UIImage.named("AddDone")
+    }
+
+    enum Favourite {
+        static let active = UIImage.named("FavouriteActive")
+        static let nonActive = UIImage.named("FavouriteNonActive")
+    }
+
+    enum Currencies {
+        static let apeCoin = UIImage.named("ApeCoin")
+        static let bitcoin = UIImage.named("Bitcoin")
+        static let cardano = UIImage.named("Cardano")
+        static let dogecoin = UIImage.named("Dogecoin")
+        static let etherium = UIImage.named("Ethereum")
+        static let shibaInu = UIImage.named("Shiba Inu")
+        static let solana = UIImage.named("Solana")
+        static let tether = UIImage.named("Tether")
     }
 }
 
