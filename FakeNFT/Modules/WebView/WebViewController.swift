964b1297477a148a92b9191f6c2b660129aafd8e
Dmitry <dmitry.nikishov.ya@yandex.ru>
2023-08-02 17:34:31 +0300

    Реализован модуль для WebView

---
+++ FakeNFT/Modules/WebView/WebViewController.swift
@@ -0,0 +1,48 @@
+//
+//  WebViewController.swift
+//  FakeNFT
+//
+//  Created by Дмитрий Никишов on 02.08.2023.
+//
+
+import Foundation
+import WebKit
+
+final class WebViewController: UIViewController {
+    private let webView = WKWebView()
+    private let viewModel: WebViewModel
+    
+    override func loadView() {
+        view = webView
+    }
+    
+    init(viewModel: WebViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError(
+            "WebViewController -> init(coder:) has not been implemented"
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        finishConfiguration()
+    }
+    
+    private func finishConfiguration() {
+        navigationController?.navigationBar.topItem?.title = " "
+        guard let url = URL(string: viewModel.url) else {
+            return
+        }
+        
+        webView.load(
+            URLRequest(
+                url: url
+            )
+        )
+        webView.allowsBackForwardNavigationGestures = true
+    }
+}
