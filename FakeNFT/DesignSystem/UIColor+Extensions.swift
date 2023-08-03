4bafc79af427334cf478906203df6305b8077d40
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-08-03 11:20:04 +0700

    Добавлен зеленый цвет

--- FakeNFT/DesignSystem/UIColor+Extensions.swift
+++ FakeNFT/DesignSystem/UIColor+Extensions.swift
@@ -12,10 +12,11 @@ extension UIColor {
     static var appBlackOnly: UIColor { UIColor(named: "AppBlackOnly") ?? .red }
     static var appBlue: UIColor { UIColor(named: "AppBlue") ?? .red }
     static var appGray: UIColor { UIColor(named: "AppGray") ?? .red }
-    static var appLightGray: UIColor { UIColor(named: "AppLightGray") ?? .red}
+    static var appLightGray: UIColor { UIColor(named: "AppLightGray") ?? .red }
     static var appRed: UIColor { UIColor(named: "AppRed") ?? .red }
     static var appWhite: UIColor { UIColor(named: "AppWhite") ?? .red }
     static var yellowUniversal: UIColor { UIColor(named: "YellowUniversal") ?? .red }
     static var lightGrey: UIColor { UIColor(named: "LightGray") ?? .red }
     static var appWhiteOnly: UIColor { UIColor(named: "AppWhiteOnly") ?? .red }
+    static var greenUniversal: UIColor { UIColor(named: "GreenUniversal") ?? .red }
 }
