0afb8abf6fe9f589254601ab4c41265ab88625b8
Bogdan <bogdan.polygalov@gmail.com>
2023-08-03 23:44:07 +0500

    Создал контроллер экрана коллекции

---
+++ FakeNFT/Epics/Catalog/CollectionScreen/CollectionScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionScreenViewController.swift
+//  FakeNFT
+//
+//  Created by Богдан Полыгалов on 03.08.2023.
+//
+
+import UIKit
+
+final class CollectionScreenViewController: UIViewController {
+    let navigationBarController = UINavigationController()
+    let image = UIImageView()
+    
+    
+}
