91fae9254c2a3adb54c8ebf0f29a442ffb944fc5
Bogdan <bogdan.polygalov@gmail.com>
2023-08-03 13:19:51 +0500

    Удалил лишний ассет, поскольку цвет можно менять в коде

--- FakeNFT/Resources/Image.swift
+++ FakeNFT/Resources/Image.swift
@@ -10,8 +10,7 @@ extension UIImage {
     static let removeFromBasket = UIImage(named: "removeFromBasket")
     static let unliked = UIImage(named: "unliked")
     static let yellowStar = UIImage(named: "yellowStar")
-    static let darkSortButton = UIImage(named: "darkSortButton")
-    static let lightSortButton = UIImage(named: "lightSortButton")
+    static let sortButton = UIImage(named: "sortButton")
 
     static let mockCell = UIImage(named: "mock.cell")
     static let mockCollection = UIImage(named: "mock.collection")
