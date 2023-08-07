d93075cf8a77476b9dc205089d0b234c7f772d21
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-08-07 10:11:23 +0700

    Блокирование UI во время загрузки корзины

--- FakeNFT/Modules/Cart/CustomViews/ProgressHUDWrapper.swift
+++ FakeNFT/Modules/Cart/CustomViews/ProgressHUDWrapper.swift
@@ -5,15 +5,21 @@
 //  Created by Aleksandr Bekrenev on 03.08.2023.
 //
 
-import Foundation
+import UIKit
 import ProgressHUD
 
 struct ProgressHUDWrapper {
+    private static var window: UIWindow? {
+        UIApplication.shared.windows.first
+    }
+
     static func show() {
+        self.window?.isUserInteractionEnabled = false
         ProgressHUD.show()
     }
 
     static func hide() {
+        self.window?.isUserInteractionEnabled = true
         ProgressHUD.dismiss()
     }
 }
