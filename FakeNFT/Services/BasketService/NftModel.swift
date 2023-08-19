eec9e2d2b214d9253dd27eb794c4fe6381b93e75
MarinaKolbina <marianna.hana@gmail.com>
2023-08-19 23:56:35 +0200

    Changes

--- FakeNFT/Services/BasketService/NftModel.swift
+++ FakeNFT/Services/BasketService/NftModel.swift
@@ -1,19 +1,19 @@
 //
-//  Models.swift
+//  NftModel.swift
 //  FakeNFT
 //
 //  Created by Marina Kolbina on 02/08/2023.
 //
 
-import UIKit
+import Foundation
 
-struct NFTModel: Codable, Equatable {
+struct NftModel: Codable, Equatable {
     let id: String
     let createdAt: String
     let name: String
     let images: [String]
     let rating: Int
     let description: String
-    let price: Double
+    let price: Float
     let author: String
 }
