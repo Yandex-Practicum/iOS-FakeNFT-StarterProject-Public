a80babccb744651dcd8eb708e1281a01aff49522
MarinaKolbina <marianna.hana@gmail.com>
2023-08-20 01:02:02 +0200

    + мелкие правки

--- FakeNFT/Epics/Basket/NetworkService/Requests/GetNFTByOrderRequest.swift
+++ FakeNFT/Epics/Basket/NetworkService/Requests/GetNFTByOrderRequest.swift
@@ -9,15 +9,13 @@ import Foundation
 
 final class GetNFTByIdRequest: NetworkRequest {
     var httpMethod: HttpMethod
-
     private let id: String
-
+    var endpoint: URL? {
+        URL(string: "https://\(Constants.host.rawValue)/api/v1/nft/\(id)")
+    }
+    
     init(id: String, httpMethod: HttpMethod) {
         self.id = id
         self.httpMethod = httpMethod
-    }
-
-    var endpoint: URL? {
-        URL(string: "https://\(Constants.host.rawValue)/api/v1/nft/\(id)")
     }
 }
