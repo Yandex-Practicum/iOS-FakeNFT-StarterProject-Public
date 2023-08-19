cfa6e205217c4583bc5d9c8756bb0bc879897f29
Bogdan <bogdan.polygalov@gmail.com>
2023-08-20 03:58:29 +0500

    Поменял название функции на более подходящие под паттерн MVP

--- FakeNFT/Services/AlertPresenter/AlertPresenterProtocol.swift
+++ FakeNFT/Services/AlertPresenter/AlertPresenterProtocol.swift
@@ -9,5 +9,5 @@ import UIKit
 
 protocol AlertPresenterProtocol {
     func injectDelegate(viewController: UIViewController)
-    func show(models: [AlertActionModel])
+    func didTapSortButton(models: [AlertActionModel])
 }
