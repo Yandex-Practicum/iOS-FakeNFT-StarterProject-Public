f9dd7f02849e79d957e0bf8c462eeac8bacfb85a
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-08-22 19:09:33 +0700

    Фикс сборки проекта

--- FakeNFT/Services/Requests/Nft/NftCollectionRequest.swift
+++ FakeNFT/Services/Requests/Nft/NftCollectionRequest.swift
@@ -4,13 +4,14 @@
 //
 //  Created by Дмитрий Никишов on 01.08.2023.
 //
+
 import Foundation
 
 struct NFTCollectionRequest: NetworkRequest {
     var httpMethod: HttpMethod = .get
-
-    var dto: Encodable?
-
+    
+    var dto: Encodable? = nil
+    
     var endpoint: URL? {
         .init(string: "https://64c51750c853c26efada7c53.mockapi.io/api/v1/collections")
     }
