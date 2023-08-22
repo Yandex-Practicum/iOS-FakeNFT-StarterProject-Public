ebadb74e8ce659ca2c0d772965cd198a8455e601
Roman Boiko <roma685235423@icloud.com>
2023-08-22 21:29:18 +0300

    Вернул функцию в AppDelegate
    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -2,6 +2,10 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         return true
+     }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
