8feafc7f7f2ce2df02f68fa256bb647fb9259de8
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-08-26 19:35:56 +0700

    Вынесена CartPaymentView

--- FakeNFT/Modules/CartPayment/CartPaymentViewController.swift
+++ FakeNFT/Modules/CartPayment/CartPaymentViewController.swift
@@ -1,52 +1,7 @@
 import UIKit
 
 public final class CartPaymentViewController: UIViewController {
-    private enum Constants {
-        static let purchaseBackgroundViewHeight: CGFloat = 186
-        static let userAgreementTextViewInsets = UIEdgeInsets(top: 12, left: 16, bottom: 126, right: 16)
-        static let purchaseButtonInsets = UIEdgeInsets(top: 16, left: 16, bottom: 50, right: 16)
-    }
-
-    private lazy var currenciesCollectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .appWhite
-        collectionView.refreshControl = self.refreshControl
-        collectionView.dataSource = self.collectionViewHelper
-        collectionView.delegate = self.collectionViewHelper
-        collectionView.register<CartPaymentCollectionViewCell>(CartPaymentCollectionViewCell.self)
-        return collectionView
-    }()
-
-    private let purchaseBackgroundView = PurchaseBackgroundView()
-
-    private lazy var userAgreementTextView: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-
-        let normalText = "CART_PAYMENT_USER_AGREEMENT_TEXTVIEW_TEXT".localized
-        let linkText = "CART_PAYMENT_USER_AGREEMENT_TEXTVIEW_LINK_TEXT".localized
-        let link = AppConstants.Links.purchaseUserAgreement
-
-        textView.addHyperLinksToText(originalText: normalText + linkText, hyperLinks: [linkText: link], lineHeight: 6)
-        textView.isEditable = false
-        textView.backgroundColor = .appLightGray
-        textView.font = .getFont(style: .regular, size: 13)
-        textView.delegate = self
-        return textView
-    }()
-
-    private lazy var purchaseButton: AppButton = {
-        let button = AppButton(type: .filled, title: "CART_PAYMENT_PURCHASE_BUTTON_TITLE".localized)
-        button.addTarget(self, action: #selector(self.didTapPurchaseButton), for: .touchUpInside)
-        return button
-    }()
-
-    private lazy var refreshControl: UIRefreshControl = {
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(self.refreshCollection(_:)), for: .valueChanged)
-        return refreshControl
-    }()
+    private let cartPaymentView = CartPaymentView()
 
     private let collectionViewHelper: CartPaymentCollectionViewHelperProtocol?
     private let viewModel: CartPaymentViewModelProtocol
@@ -69,6 +24,10 @@ public final class CartPaymentViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    public override func loadView() {
+        self.view = self.cartPaymentView
+    }
+
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.configure()
@@ -89,101 +48,29 @@ extension CartPaymentViewController: CartPaymentCollectionViewHelperDelegate {
     }
 }
 
-// MARK: - UITextViewDelegate
-extension CartPaymentViewController: UITextViewDelegate {
-    public func textView(
-        _ textView: UITextView,
-        shouldInteractWith URL: URL,
-        in characterRange: NSRange
-    ) -> Bool {
-        self.router.showUserAgreementWebView(on: self, urlString: URL.absoluteString)
-        return false
-    }
-}
-
 private extension CartPaymentViewController {
     func configure() {
-        self.view.backgroundColor = .appWhite
-
-        self.addSubviews()
-        self.addConstraints()
         self.bind()
+        self.configureView()
         self.setupNavigationBar()
 
-        ProgressHUDWrapper.show()
         self.viewModel.fetchCurrencies()
-    }
-
-    func addSubviews() {
-        self.view.addSubview(self.currenciesCollectionView)
-        self.view.addSubview(self.purchaseBackgroundView)
-
-        self.purchaseBackgroundView.addSubview(self.userAgreementTextView)
-        self.purchaseBackgroundView.addSubview(self.purchaseButton)
-    }
-
-    func addConstraints() {
-        NSLayoutConstraint.activate([
-            self.currenciesCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            self.currenciesCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.currenciesCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.currenciesCollectionView.bottomAnchor.constraint(equalTo: self.purchaseBackgroundView.topAnchor),
-
-            self.purchaseBackgroundView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.purchaseBackgroundView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.purchaseBackgroundView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            self.purchaseBackgroundView.heightAnchor.constraint(
-                equalToConstant: Constants.purchaseBackgroundViewHeight
-            ),
-
-            self.userAgreementTextView.topAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.topAnchor,
-                constant: Constants.userAgreementTextViewInsets.top
-            ),
-            self.userAgreementTextView.leadingAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.leadingAnchor,
-                constant: Constants.userAgreementTextViewInsets.left
-            ),
-            self.userAgreementTextView.trailingAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.trailingAnchor,
-                constant: -Constants.userAgreementTextViewInsets.right
-            ),
-            self.userAgreementTextView.bottomAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.bottomAnchor,
-                constant: -Constants.userAgreementTextViewInsets.bottom
-            ),
-
-            self.purchaseButton.topAnchor.constraint(
-                equalTo: self.userAgreementTextView.bottomAnchor,
-                constant: Constants.purchaseButtonInsets.top
-            ),
-            self.purchaseButton.leadingAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.leadingAnchor,
-                constant: Constants.purchaseButtonInsets.left
-            ),
-            self.purchaseButton.trailingAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.trailingAnchor,
-                constant: -Constants.purchaseButtonInsets.right
-            ),
-            self.purchaseButton.bottomAnchor.constraint(
-                equalTo: self.purchaseBackgroundView.bottomAnchor,
-                constant: -Constants.purchaseButtonInsets.bottom
-            )
-        ])
     }
 
     func setupNavigationBar() {
         let font = UIFont.getFont(style: .bold, size: 17)
         let textColor = UIColor.appBlack
-        let titleAttributes = [NSAttributedString.Key.font: font,
-                               NSAttributedString.Key.foregroundColor: textColor]
+        let titleAttributes = [
+            NSAttributedString.Key.font: font,
+            NSAttributedString.Key.foregroundColor: textColor
+        ]
         self.navigationController?.navigationBar.titleTextAttributes = titleAttributes
         self.navigationItem.title = "CART_PAYMENT_VIEW_TITLE".localized
     }
 
     func bind() {
         self.viewModel.currencies.bind { [weak self] _ in
-            self?.currenciesCollectionView.reloadData()
+            self?.cartPaymentView.updateCollection()
         }
 
         self.viewModel.cartPaymentViewState.bind { state in
@@ -215,18 +102,21 @@ private extension CartPaymentViewController {
             self.router.showErrorAlert(on: self, error: error)
         }
     }
-}
 
-// MARK: - Actions
-private extension CartPaymentViewController {
-    @objc
-    func didTapPurchaseButton() {
-        self.viewModel.purсhase()
-    }
+    func configureView() {
+        self.cartPaymentView.collectionHelper = self.collectionViewHelper
 
-    @objc
-    func refreshCollection(_ sender: UIRefreshControl) {
-        self.viewModel.fetchCurrencies()
-        sender.endRefreshing()
+        self.cartPaymentView.onTapPurchaseButton = { [weak self] _ in
+            self?.viewModel.purсhase()
+        }
+
+        self.cartPaymentView.onRefreshCollection = { [weak self] _ in
+            self?.viewModel.fetchCurrencies()
+        }
+
+        self.cartPaymentView.onOpenUserAgreement = { [weak self] url in
+            guard let self = self else { return }
+            self.router.showUserAgreementWebView(on: self, urlString: url.absoluteString)
+        }
     }
 }
