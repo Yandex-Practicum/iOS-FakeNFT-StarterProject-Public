b9e46462062589df6d4a22b16696c83276b14a42
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-08-27 03:38:00 +0300

    Added property wrapper for implementation of bindings

---
+++ FakeNFT/Presentation/Observable.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+@propertyWrapper
+final class Observable<Value> {
+    
+    private var onChange: ((Value) -> Void)? = nil
+    
+    var wrappedValue: Value {
+        didSet {
+            onChange?(wrappedValue)
+        }
+    }
+    
+    var projectedValue: Observable<Value> { self }
+    
+    init(wrappedValue: Value) {
+        self.wrappedValue = wrappedValue
+    }
+    
+    func bind(action: @escaping (Value) -> Void) {
+        self.onChange = action
+    }
+}
