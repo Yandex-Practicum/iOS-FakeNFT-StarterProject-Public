9fe6e8e3c18530f3208f4d30683659c86bc63c08
Bogdan <bogdan.polygalov@gmail.com>
2023-08-29 00:04:45 +0500

    Поменяли реализацию

--- FakeNFT/Services/BasketService/CurrenciesService.swift
+++ FakeNFT/Services/BasketService/CurrenciesService.swift
@@ -12,20 +12,21 @@ protocol CurrenciesServiceProtocol {
 }
 
 final class CurrenciesService: CurrenciesServiceProtocol {
+    static let shared = CurrenciesService()
     private let networkClient: NetworkClient
     private var currencyTask: NetworkTask?
-    
+
     init(networkClient: NetworkClient = DefaultNetworkClient()) {
         self.networkClient = networkClient
     }
-    
+
     func load(completion: @escaping (Result<[CurrencyModel], Error>) -> Void) {
         assert(Thread.isMainThread)
-        
+
         if currencyTask != nil {
             return
         }
-        
+
         let getCurrenciesRequest = GetCurrenciesRequest(httpMethod: .get)
         currencyTask = networkClient.send(request: getCurrenciesRequest, type: [CurrencyModel].self) { result in
             switch result {
