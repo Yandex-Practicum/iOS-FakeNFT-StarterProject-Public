485be83495ab441e990642246f8df7b1af3318e9
Art Hack <art.hack.ios@gmail.com>
2023-08-30 17:28:17 +0300

    Удалил сториборд. Сверстал Tabbar кодом.

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -3,5 +3,10 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {}
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = TabBarViewController()
+        window?.makeKeyAndVisible()
+    }
 }
---
+++ FakeNFT/TabBatViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        generateTabBar()
+    }
+    
+    private func generateTabBar() {
+        viewControllers = [
+        generateVC(viewController: ProfileViewController(), title: "Профиль", image: UIImage(systemName: "person.crop.circle.fill")),
+        generateVC(viewController: CatalogViewController(), title: "Каталог", image: UIImage(systemName: "rectangle.stack.fill")),
+        generateVC(viewController: CartViewController(), title: "Корзина", image: UIImage(systemName: "bag")),
+        generateVC(viewController: StatisticViewController(), title: "Статистика", image: UIImage(systemName: "flag.2.crossed.fill"))
+        ]
+    }
+    
+    private func generateVC(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+        viewController.tabBarItem.title = title
+        viewController.tabBarItem.image = image
+        return UINavigationController(rootViewController: viewController)
+    }
+}
+
