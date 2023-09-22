a3c5fb21595eb83296cb0e9eb903ff4f86417f76
Igor Polunin <garry.818@yandex.ru>
2023-09-22 13:13:58 +0300

    Добавлен тест в testProfile

--- FakeNFTUITests/FakeNFTUITests.swift
+++ FakeNFTUITests/FakeNFTUITests.swift
@@ -1,21 +1,21 @@
 import XCTest
 
 final class ImageFeedzUITests: XCTestCase {
-
-    private let app = XCUIApplication() // переменная приложения
-
+    
+    private let app = XCUIApplication() // Переменная приложения
+    
     override func setUpWithError() throws {
-        continueAfterFailure = false // настройка выполнения тестов, которая прекратит выполнения тестов, если в тесте что-то пошло не так
-
+        continueAfterFailure = false // Настройка выполнения тестов, которая прекратит выполнения тестов, если в тесте что-то пошло не так
+        
         app.launch() // запускаем приложение перед каждым тестом
     }
-
+    
     func testMyNft() throws {
         // тестируем Мои НФТ
         // Подождать, пока открывается и загружается экран профиля
         // нажать на ячейку таблицы Мои НФТ
         // Сделать жест «смахивания» вверх по экрану
-
+        
         // Поставить лайк в ячейке  картинки
         // Отменить лайк в ячейке  картинки
         // Нажать на экран сортиировки
@@ -25,26 +25,26 @@ final class ImageFeedzUITests: XCTestCase {
         // Нажать на экран сортиировки
         // нажать на всплывающем окне алерта кнопку сортировки по рейтингу
         // Вернуться на экран профиля
-
+        
         let tablesQuery = app.tables
         // перейти на экран мио нфт
-
+        
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0) // берем ячейку по индексу 0
         XCTAssertTrue(cell.waitForExistence(timeout: 5))// подождёт появления ячейки на экране в течение 5 секунд.
         cell.tap()
         cell.swipeUp() // Сделать жест «смахивания» вверх по экрану
-
+        
         sleep(2)
-
+        
         let cellToLike = tablesQuery.children(
             matching: .cell
         ).element(boundBy: 1) // выбираем верхнюю первую ячейку=картинку
-
+        
         // нажать на кнопку лайкаа
         cellToLike.buttons["likeButtonTapped"].tap()
         sleep(2)
         cellToLike.buttons["likeButtonTapped"].tap()
-
+        
         sleep(3)
         // нажать на экран сортировки
         app.navigationBars.buttons.element(boundBy: 1).tap()
@@ -65,60 +65,90 @@ final class ImageFeedzUITests: XCTestCase {
         app.buttons["ratingSorting"].tap()
         sleep(3)
         app.navigationBars.buttons.element(boundBy: 0).tap()
-
+        
     }
-
+    
     func testАavouritesNft() throws {
         // тестируем избранные НФТ
         // Подождать, пока открывается и загружается экран профиля
         // нажать на ячейку таблицы избранные НФТ
-
+        
         // Нажать лайк в ячейке  картинки
         // Вернуться на экран профиля
-
+        
         let tablesQuery = app.tables
-        // перейти на экран избранные нфт
-
-        let cell = tablesQuery.children(matching: .cell).element(boundBy: 1) // берем ячейку по индексу 0
-        XCTAssertTrue(cell.waitForExistence(timeout: 5))// подождёт появления ячейки на экране в течение 5 секунд.
+        // Перейти на экран избранные нфт
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 1) // Берем ячейку по индексу 0
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))// Подождёт появления ячейки на экране в течение 5 секунд.
         cell.tap()
-
         sleep(2)
         let cellToLike = app.collectionViews.element.cells.element(boundBy: 0)
-        // нажать на кнопку лайкаа
+        // Нажать на кнопку лайка
         cellToLike.buttons["likeButtonTapped"].tap()
         sleep(2)
         app.navigationBars.buttons.element(boundBy: 0).tap()
-
+        
     }
-
+    
     func testProfile() throws {
-        // тестируем сценарий профиля
+        // Тестируем сценарий профиля
         // Подождать, пока открывается и загружается экран профиля
         // Перейти на экран EditProfileViewController профиля
         // Проверить, что на нём отображаются ваши персональные данные
-        // нажать кнопку добавить аватар
-        // нажать кнопку Ок на всплывающем алерте
+        // Нажать кнопку добавить аватар
+        // Нажать кнопку Ок на всплывающем алерте
         // Нажать кнопку логаута
         // Проверить, что открылся экран профиля
         app.navigationBars.buttons.element(boundBy: 0).tap()
         sleep(3)
+        
+        // Ввод текста в поле nameTextField
+        let nameTextField = app.textFields["nameTextField"]
+        nameTextField.tap()
+        nameTextField.press(forDuration: 2)
+        app.menuItems["Select All"].tap()
+        nameTextField.typeText("Студентус практикус")
+        
+        // Подождите 2 секунды
+        sleep(2)
+        // Ввод текста в поле описание
+        let descriptionTextField = app.textViews["descriptionTextField"]
+        descriptionTextField.tap()
+        app.menuItems["Select All"].tap()
+        descriptionTextField.typeText("Прошел 5-й спринт, и этот пройду")
+        
+        // Ввод текста в поле ЮРЛ
+        let urlTextField = app.textFields["urlTextField"]
+        urlTextField.tap()
+        urlTextField.press(forDuration: 2)
+        app.menuItems["Select All"].tap()
+        urlTextField.typeText("https://practicum.yandex.ru/ios-developer")
+        
+        // Нажимаем на кнопку сменить аватар
+        app.buttons["changeAvatarButton"].tap()
+        sleep(3)
+        
+        // Вводим тестовый аватар
+        let alertTextfield = app.alerts.textFields.element
+        alertTextfield.tap()
+        sleep(3)
+        alertTextfield.typeText("https://code.s3.yandex.net/landings-v2-ios-developer/space.PNG")
+        sleep(3)
+        // Нажимаем Ок кнопки алерта
+        
+        app.buttons["showErrorAlertDoOk"].tap()
+        sleep(3)
+        
+        // Проверка существования всех полей
         XCTAssertTrue(app.images["profileImage"].exists)
-
         XCTAssertTrue(app.staticTexts["nameLabel"].exists)
         XCTAssertTrue(app.staticTexts["descriptionLabel"].exists)
         XCTAssertTrue(app.textViews["descriptionTextField"].exists)
         XCTAssertTrue(app.staticTexts["urlLabel"].exists)
         XCTAssertTrue(app.textFields["urlTextField"].exists)
-
         sleep(3)
-
-        app.buttons["changeAvatarButton"].tap()
-        sleep(3)
-        app.buttons["showErrorAlertDoOk"].tap()
-        sleep(3)
-
+        
+        //Нажатие на кнопку закрытия окна редактирования профиля
         app.buttons["closeButtonTapped"].tap()
     }
-
 }
