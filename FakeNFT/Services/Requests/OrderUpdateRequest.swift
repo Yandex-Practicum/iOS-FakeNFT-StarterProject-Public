089899d5430a2ed1ead725b876545b2d177293b7
Art Hack <art.hack.ios@gmail.com>
2023-09-29 08:58:32 +0300

    /

--- FakeNFT/Services/Requests/OrderUpdateRequest.swift
+++ FakeNFT/Services/Requests/OrderUpdateRequest.swift
@@ -1,8 +1,21 @@
-//
-//  OrderUpdateRequest.swift
-//  FakeNFT
-//
-//  Created by arthack on 22.09.2023.
-//
-
 import Foundation
+
+struct OrderUpdateRequest: NetworkRequest {
+    let orderUpdateDTO: OrderUpdateDTO
+    var endpoint: URL? {
+        URL(string: "https://64e7948bb0fd9648b7902415.mockapi.io/api/v1/orders/1")
+    }
+    
+    var httpMethod: HttpMethod {
+        .put
+    }
+    
+    var dto: Encodable? {
+        orderUpdateDTO
+    }
+}
+
+struct OrderUpdateDTO: Encodable {
+    let nfts: [String]
+    let id: String
+}
