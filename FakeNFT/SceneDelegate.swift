931059652716aeb40609754bfea74bad0a533adf
Artem <kohanartem@gmail.com>
2023-10-06 11:55:32 +0300

    Первичный функционирующий таббар контроллер

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
