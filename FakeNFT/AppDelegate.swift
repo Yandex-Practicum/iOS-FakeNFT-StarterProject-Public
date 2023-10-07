e42dd300a2e1641311cf0fd0f5c80e0dbfd5b5fb
Aleksei Tinkov <boofle@outlook.com>
2023-10-07 12:13:15 +0300

    Добавлен TabBar в коде и цвета

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -1,14 +1,16 @@
 import UIKit
 
-@main
+@UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
