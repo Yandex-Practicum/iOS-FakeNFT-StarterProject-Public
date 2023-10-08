7b1cb151cc3ea99a6a93a72978cb2af0f5347db0
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-10-08 22:57:03 +0500

    StatisticsViewController переместил в папку Rating

--- FakeNFT/Statistics/StatisticsViewController.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-final class StatisticsViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .nftWhite
-    }
-}
