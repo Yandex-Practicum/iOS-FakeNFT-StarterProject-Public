9df84afadac210ef518264c0e9b3ad6e623ebf9d
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-11-12 21:52:22 +0300

    заглушка

---
+++ FakeNFT/Profile/ProfileScreen/Views/EditScreen/UserNFTViewController/FavoritesNFTScreen/FavoritesNFTViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class FavoritesNFTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .nftWhite
+    }
+}
