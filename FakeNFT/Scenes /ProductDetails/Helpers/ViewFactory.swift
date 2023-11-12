bf48c262c9f353963165c250919df1d277f20cf9
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-11-12 22:49:50 +0300

    создание и настройка элементов пользовательского интерфейса

---
+++ FakeNFT/Scenes /ProductDetails/Helpers/ViewFactory.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+
+final class ViewFactory {
+    func createTextLabel() -> UILabel {
+        let label = UILabel()
+        label.font = .headline2
+        return label
+    }
+
+    func createTextView() -> UITextView {
+        let textView = UITextView()
+        textView.isScrollEnabled = false
+        textView.font = .bodyRegular
+        textView.backgroundColor = .nftLightgrey
+        textView.layer.cornerRadius = 12
+        textView.textContainerInset = UIEdgeInsets(top: 11, left: 10, bottom: 11, right: 10)
+        return textView
+    }
+}
