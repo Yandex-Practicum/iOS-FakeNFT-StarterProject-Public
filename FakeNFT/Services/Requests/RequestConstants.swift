2e748e3d31c95dfe0291187b8ea2e41ccaca803e
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2023-11-13 21:50:35 +0300

    SwiftLint disabled Enable user sandboxing - NO

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,4 +1,4 @@
 enum RequestConstants {
-    #warning("insert your baseUrl <id>.mockapi.io")
+    //insert your baseUrl <id>.mockapi.io
     static let baseURL = "https://65450baa5a0b4b04436d87e6.mockapi.io"
 }
