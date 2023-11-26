a065605357d6daa8903064eda631f343cc474149
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-11-26 14:53:36 +0300

    Fix багов

--- FakeNFT/Helpers/ViewControllerFactory.swift
+++ FakeNFT/Helpers/ViewControllerFactory.swift
@@ -3,21 +3,20 @@ import UIKit
 final class ViewControllerFactory {
     func makeWebView(url: URL) -> WebViewViewController {
         let controller = WebViewViewController(url: url)
-        
         return controller
     }
-
+    
     func makeUserNFTViewController(nftList: [String]) -> UserNFTViewController {
         let userNFTViewController = UserNFTViewController(nftList: nftList,
                                                           viewModel: UserNFTViewModel(model: UserNFTModel()))
         return userNFTViewController
     }
-
+    
     func makeFavoritesNFTViewController() -> FavoritesNFTViewController {
         return FavoritesNFTViewController()
     }
-
-    func makeEditingViewController(viewModel: ProfileViewModelProtocol) -> EditingViewController {
-        return EditingViewController(viewModel: viewModel as! EditingViewModelProtocol)
+    
+    func makeEditingViewController() -> EditingViewController {
+        return EditingViewController(viewModel: EditingViewModel(profileService: ProfileService(networkClient: DefaultNetworkClient())))
     }
 }
