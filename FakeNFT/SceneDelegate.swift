8605797aa8857a5b8c4df59f7c84eb5dca6ff1e5
Eldar Aidumov <superaydumov@me.com>
2023-12-11 17:34:03 +0300

    Storyboard was deleted from the app. SceneDelegate was modified.

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -8,8 +8,15 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         nftStorage: NftStorageImpl()
     )
 
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        let tabBarController = window?.rootViewController as? TabBarController
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        
+        let tabBarController = window.rootViewController as? TabBarController
         tabBarController?.servicesAssembly = servicesAssembly
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
