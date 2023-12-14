1f26ef6b9566f65a08cb802c0f8530933a3e2201
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-12-14 15:17:19 +0500

    Добавлены модели данных: NFT, Author и UserProfile. Все структуры соответствуют Codable для работы с JSON. Включают поля для основной информации, включая имена, изображения, описание, веб-сайты и уникальные идентификаторы

---
+++ FakeNFT/Models/Network/NFT.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct NFT: Codable {
+    let createdAt: String
+    let name: String
+    let images: [String]
+    let rating: Int
+    let description: String
+    let price: Float
+    let author: String
+    let id: String
+}
---
+++ FakeNFT/Models/Network/UserProfile.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct UserProfile: Codable {
+    let name: String
+    let avatar: String
+    let description: String
+    let website: String
+    let nfts: [String]
+    let likes: [String]
+    let id: String
+}
+
