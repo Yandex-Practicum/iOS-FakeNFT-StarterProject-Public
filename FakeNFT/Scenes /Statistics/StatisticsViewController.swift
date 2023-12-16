48b869d377f62b262217e37be38affea9e0e5ead
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-12-16 21:45:09 +0300

    Rename

--- FakeNFT/Scenes /Statistics/StatisticsViewController.swift
+++ FakeNFT/Scenes /Statistics/StatisticsViewController.swift
@@ -77,7 +77,7 @@ final class StatisticsViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .background
         addSubviews()
-        setupConstraints()
+        makeConstraints()
     }
 
     @objc
@@ -106,7 +106,7 @@ final class StatisticsViewController: UIViewController {
         view.addSubview(tableView)
     }
 
-    private func setupConstraints() {
+    private func makeConstraints() {
         NSLayoutConstraint.activate(
             [
                 sortButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 44 + 2),
