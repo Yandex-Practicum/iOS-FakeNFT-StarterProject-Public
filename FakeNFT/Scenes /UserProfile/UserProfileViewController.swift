dfb8dcfc1a890212de168fcf9317040ae03decc2
Andrey Paramonov <a.paramonov@dodopizza.com>
2023-12-16 19:32:41 +0300

    Add vc and vm

---
+++ FakeNFT/Scenes /UserProfile/UserProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+// Created by Андрей Парамонов on 16.12.2023.
+//
+
+import Foundation
+
+class UserProfileViewController {
+}
---
+++ FakeNFT/Scenes /UserProfile/UserProfileViewModel.swift
@@ -0,0 +1,11 @@
+//
+// Created by Андрей Парамонов on 16.12.2023.
+//
+
+import Foundation
+
+protocol UserProfileViewModel {
+}
+
+final class UserProfileViewModelImpl: UserProfileViewModel {
+}
