6a800562362f437b6310f549f9251ddadc2b82f0
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-12-29 01:10:13 +0500

    сборка приложения, приведение в соответсвие

---
+++ FakeNFT/Scenes /Statistic/StatisticsViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class StatisticsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .nftWhite
+    }
+}
---
+++ FakeNFT/Services/NFTServiceProfile.swift
@@ -0,0 +1,8 @@
+//
+//  NFTServiceProfile.swift
+//  FakeNFT
+//
+//  Created by Алексей Тиньков on 28.12.2023.
+//
+
+import Foundation
