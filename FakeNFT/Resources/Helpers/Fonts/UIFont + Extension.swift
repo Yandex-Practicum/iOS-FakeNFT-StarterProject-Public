e6207b2af4abddfe46c4f04c3afe110926aff1e8
iamjohansson <ma4ujloba@gmail.com>
2024-01-13 19:58:56 +0300

    Добавлены основные элементы интерфейса

---
+++ FakeNFT/Resources/Helpers/Fonts/UIFont + Extension.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UIFont {
+    
+    static var sfProBold22 = UIFont(name: "SFProText-Bold", size: 22)
+    static var sfProRegular13 = UIFont(name: "SFProText-Regular", size: 13)
+    static var sfProRegular15 = UIFont(name: "SFProText-Regular", size: 15)
+}
--- FakeNFT/Scenes /Profile/ProfileViewController.swift
+++ FakeNFT/Scenes /Profile/ProfileViewController.swift
@@ -24,13 +24,47 @@ final class ProfileViewController: UIViewController {
     private lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        
+        label.font = .sfProBold22
+        label.textColor = .ypBlack
         return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .sfProRegular13
+        label.textColor = .ypBlack
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var webLinkLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .ypBlueUn
+        label.font = .sfProRegular15
+        label.isUserInteractionEnabled = true
+        label.text = "https://github.com/iamjohansson"
+        return label
+    }()
+    
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(systemName: "square.and.pencil")
+        button.setImage(image, for: .normal)
+        button.tintColor = .ypBlack
+        button.addTarget(self, action: #selector(didTapEditButton), for: .touchUpInside)
+        return button
     }()
     
     // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .ypWhite
     }
     
+    @objc func didTapEditButton() {
+        // TODO: на экран редактирования
+    }
 }
