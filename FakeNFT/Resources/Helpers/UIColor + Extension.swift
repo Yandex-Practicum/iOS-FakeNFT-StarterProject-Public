d5564c2eb3f75e146ebf812e650215f74bf56d43
iamjohansson <ma4ujloba@gmail.com>
2024-01-13 16:52:04 +0300

    Добавлены UIColors и назначены статик свойста Добавлены иконки для таббара

---
+++ FakeNFT/Resources/Helpers/UIColor + Extension.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension UIColor {
+    
+    // MARK: Default colors
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+    static var ypLightGray: UIColor { UIColor(named: "ypLightGray") ?? UIColor.lightGray }
+    
+    // MARK: Universal colors
+    static var ypBackgroundUn: UIColor { UIColor(named: "ypBackgroundUn") ?? UIColor.gray }
+    static var ypBlackUn: UIColor { UIColor(named: "ypBlackUn") ?? UIColor.black }
+    static var ypBlueUn: UIColor { UIColor(named: "ypBlueUn") ?? UIColor.blue }
+    static var ypGrayUn: UIColor { UIColor(named: "ypGrayUn") ?? UIColor.gray }
+    static var ypGreenUn: UIColor { UIColor(named: "ypGreenUn") ?? UIColor.green }
+    static var ypRedUn: UIColor { UIColor(named: "ypRedUn") ?? UIColor.red }
+    static var ypWhiteUn: UIColor { UIColor(named: "ypWhiteUn") ?? UIColor.white }
+    static var ypYellowUn: UIColor { UIColor(named: "ypYellowUn") ?? UIColor.yellow }
+
+}
