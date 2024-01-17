300e0fd10314352b3849e8b7ee5c86f7f43b8a2c
iamjohansson <ma4ujloba@gmail.com>
2024-01-17 13:28:10 +0300

    fix (delete comments)

--- FakeNFT/Services/ProfileService.swift
+++ FakeNFT/Services/ProfileService.swift
@@ -40,22 +40,4 @@ final class ProfileService: ProfileServiceProtocol {
             }
         }
     }
-
-//    func loadNft(id: String, completion: @escaping NftCompletion) {
-//        if let nft = storage.getNft(with: id) {
-//            completion(.success(nft))
-//            return
-//        }
-//
-//        let request = NFTRequest(id: id)
-//        networkClient.send(request: request, type: Nft.self) { [weak storage] result in
-//            switch result {
-//            case .success(let nft):
-//                storage?.saveNft(nft)
-//                completion(.success(nft))
-//            case .failure(let error):
-//                completion(.failure(error))
-//            }
-//        }
-//    }
 }
