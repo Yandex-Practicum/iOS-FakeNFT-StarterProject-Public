733a0a1456be8ef6bf96efbe85afb47d475bb851
GoncharovNG <goncharov.ng@yandex.ru>
2024-03-06 23:48:14 +0300

    Мердж

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -4,56 +4,32 @@ final class TabBarController: UITabBarController {
 
     var servicesAssembly: ServicesAssembly!
 
-    private let profileTabBarItem = UITabBarItem(
-            title: NSLocalizedString("Tab.profile", comment: ""),
-            image: UIImage(systemName: "person.crop.circle.fill"),
-            tag: 0
-    )
-
     private let catalogTabBarItem = UITabBarItem(
             title: NSLocalizedString("Tab.catalog", comment: ""),
             image: UIImage(systemName: "square.stack.fill"),
             tag: 0
     )
-
-    private let basketTabBarItem = UITabBarItem(
-            title: NSLocalizedString("Tab.basket", comment: ""),
-            image: UIImage(systemName: "basket.fill"),
-            tag: 0
-    )
-
-    private let statisticsTabBarItem = UITabBarItem(
-            title: NSLocalizedString("Tab.statistics", comment: ""),
-            image: UIImage(systemName: "flag.2.crossed.fill"),
-            tag: 0
-    )
-
-
+    
+    private let cardTabBarItem = UITabBarItem(
+           title: NSLocalizedString("Tab.cart", comment: ""),
+           image: .cardActiveIcon,
+           tag: 1
+       )
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let profileViewController = TestCatalogViewController(
-                servicesAssembly: servicesAssembly
-        )
-
         let catalogController = TestCatalogViewController(
-                servicesAssembly: servicesAssembly
+            servicesAssembly: servicesAssembly
         )
-
-        let basketController = TestCatalogViewController(
+        let cardController = CartFactory().create(
+            with: .init(
                 servicesAssembly: servicesAssembly
+            )
         )
-
-        let statisticsController = TestCatalogViewController(
-                servicesAssembly: servicesAssembly
-        )
-
-        profileViewController.tabBarItem = profileTabBarItem
         catalogController.tabBarItem = catalogTabBarItem
-        basketController.tabBarItem = basketTabBarItem
-        statisticsController.tabBarItem = statisticsTabBarItem
-
-        viewControllers = [profileViewController, catalogController, basketController, statisticsController]
+        cardController.tabBarItem = cardTabBarItem
+        
+        viewControllers = [catalogController, cardController]
 
         view.backgroundColor = .systemBackground
     }
