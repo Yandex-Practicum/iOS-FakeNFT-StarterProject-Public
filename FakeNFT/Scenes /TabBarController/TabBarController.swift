a46510447df272b126bb16d2d77d32ed9f65bc61
ospanovadinara <ospan.dinara@yandex.ru>
2024-03-22 21:51:40 +0500

    удалила библиотеки и добавила по новой

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -14,23 +14,23 @@ final class TabBarController: UITabBarController {
         self.servicesAssembly = servicesAssembly
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let catalogController = createNavigation(with: L10n.Tab.catalog,
                                                  and: UIImage(systemName: "square.stack.3d.up.fill"),
                                                  vc: TestCatalogViewController(servicesAssembly: servicesAssembly))
-        
+
         self.setViewControllers([catalogController], animated: true)
 
         view.backgroundColor = .systemBackground
     }
-    
+
     private func createNavigation(with title: String,
                                   and image: UIImage?,
                                   vc: UIViewController) -> UINavigationController {
@@ -39,7 +39,7 @@ final class TabBarController: UITabBarController {
         nav.tabBarItem.title = title
         nav.tabBarItem.image = image
         nav.viewControllers.first?.navigationItem.title = title
-        
+
         return nav
     }
 }
