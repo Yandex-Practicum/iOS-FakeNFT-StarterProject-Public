76779f481a718dc2bc8412f25f0e671ecb44d001
Leo <leo-turko@mail.ru>
2024-03-27 21:16:52 +0300

    Add Fonts

---
+++ FakeNFT/Resources/Helpers/Fonts/UIFont+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+Extension.swift
+//  FakeNFT
+//
+//  Created by Леонид Турко on 27.03.2024.
+//
+
+import UIKit
+
+extension UIFont {
+    static var sfProBold22 = UIFont(name: "SFProText-Bold", size: 22)
+    static var sfProBold17 = UIFont(name: "SFProText-Bold", size: 17)
+    static var sfProRegular13 = UIFont(name: "SFProText-Regular", size: 13)
+    static var sfProRegular15 = UIFont(name: "SFProText-Regular", size: 15)
+    static var sfProLight10 = UIFont(name: "SFProText-Regular", size: 10)
+    static var sfProRegular17 = UIFont(name: "SFProText-Regular", size: 17)
+    static var sfProMedium10 = UIFont(name: "SFProText-Medium", size: 10)
+}
