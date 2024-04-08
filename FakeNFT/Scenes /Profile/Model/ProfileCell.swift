32524ce1bc74f104b26d41100a0341af112f99b1
bestNewcomer <NewcomerRUS@yandex.ru>
2024-04-08 18:36:01 +0300

    Добавил страницу профиля

---
+++ FakeNFT/Scenes /Profile/Model/ProfileCell.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileCell.swift
+//  FakeNFT
+//
+//  Created by Ринат Шарафутдинов on 08.04.2024.
+//
+
+import Foundation
---
+++ FakeNFT/Scenes /Profile/Model/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  FakeNFT
+//
+//  Created by Ринат Шарафутдинов on 08.04.2024.
+//
+
+import Foundation
