6243d232902d6e7ccb996a23e4be41174e9a2a14
Nataliya Trufanova <nataliya.truffez@gmail.com>
2024-06-25 09:42:09 +0200

    feat: Добавлены новые свойства в модель Nft

--- FakeNFT/Models/Network/Nft.swift
+++ FakeNFT/Models/Network/Nft.swift
@@ -2,5 +2,11 @@ import Foundation
 
 struct Nft: Decodable {
     let id: String
+    let name: String
     let images: [URL]
+    let rating: Int
+    let description: String
+    let price: Double
+    let author: String
+    let createdAt: String
 }
