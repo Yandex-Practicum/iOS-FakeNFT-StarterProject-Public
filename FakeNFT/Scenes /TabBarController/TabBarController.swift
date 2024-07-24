2b9d394dad3a9831b6cf163cd9c767e334653785
Vladimir Vinakheras <vvinageras@gmail.com>
2024-07-24 17:27:21 +0200

    Cleaning epic Deleted unused code and tabBarItem from other epics

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -3,13 +3,6 @@ import UIKit
 final class TabBarController: UITabBarController {
 
     var servicesAssembly: ServicesAssembly!
-
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
-    )
-    
     private let statisticsTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.statistics", comment: ""),
         image: Asset.Images.tabBarStatistic,
@@ -18,16 +11,12 @@ final class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
-        )
+        
         let statisticsController = StatisticsViewController()
         statisticsController.tabBarItem = statisticsTabBarItem
-        catalogController.tabBarItem = catalogTabBarItem
+        
+        viewControllers = [statisticsController]
 
-        viewControllers = [catalogController, statisticsController]
-
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .nftWhite
     }
 }
--- FakeNFT/Services/ModulesAssembly.swift
+++ FakeNFT/Services/ModulesAssembly.swift
@@ -17,13 +17,9 @@ final class ModulesAssembly: ModulesAssemblyProtocol {
     static func mainScreenBuilder() -> UIViewController {
         let tabbarController = UITabBarController()
         
-        let profileViewController = Self.profileScreenBuilder()
         let statisticsViewController = Self.statisticsScreenBuilder()
         
-        profileViewController.tabBarItem = UITabBarItem(
-            title: NSLocalizedString("Профиль", comment: ""),
-            image: UIImage(systemName: "person.crop.circle.fill"),
-            tag: 0)
+   
         
         statisticsViewController.tabBarItem = UITabBarItem(
             title: NSLocalizedString("Статистика", comment: ""),
@@ -31,17 +27,9 @@ final class ModulesAssembly: ModulesAssemblyProtocol {
             tag: 3)
         
         
-        tabbarController.viewControllers = [profileViewController, statisticsViewController]
+        tabbarController.viewControllers = [statisticsViewController]
         
         return tabbarController
-    }
-    
-    static func profileScreenBuilder() -> UIViewController {
-        let profileViewController = ProfileViewController()
-        let profilePresenter = ProfilePresenter(view: profileViewController)
-        profileViewController.presenter = profilePresenter
-        
-        return profileViewController
     }
     
     static func statisticsScreenBuilder() -> UIViewController {
