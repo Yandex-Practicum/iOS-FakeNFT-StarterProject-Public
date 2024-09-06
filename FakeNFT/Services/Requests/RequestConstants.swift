2edd2fb8afb58108854fb7a72632f81ee7ad5c9f
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-09-06 22:45:58 +0300

    Добавил токен

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-    #warning("Instert your token here")
-    static let token = ""
+    static let token = "54109550-3338-457f-81d5-2e54d69e7125"
 }
