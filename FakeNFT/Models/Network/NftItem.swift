f079f0fb85fde24caa52ce516de85fcd9256dcc4
Aleksandr Salagubov <salagubovalex@yandex-team.ru>
2024-09-09 23:19:45 +0300

    card_1: Added ViewModel and ViewController and TableViewCell

---
+++ FakeNFT/Models/Network/NftItem.swift
@@ -0,0 +1,16 @@
+//
+//  Cart.swift
+//  FakeNFT
+//
+//  Created by Alexander Salagubov on 09.09.2024.
+//
+
+import Foundation
+import UIKit
+
+struct NftItem {
+  let image: UIImage
+  let name: String
+  let rating: Int
+  let price: Double
+}
---
+++ FakeNFT/Scenes /Cart/Payment/PaymentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentViewController.swift
+//  FakeNFT
+//
+//  Created by Alexander Salagubov on 09.09.2024.
+//
+
+import Foundation
+import UIKit
+
+final class PaymentViewController: UIViewController {
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+}
