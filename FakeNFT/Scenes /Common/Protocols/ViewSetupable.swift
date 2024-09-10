fd8e631bb290d257e7c1594785cb3a2e832638fd
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-09-10 13:17:15 +0300

    Добавил протокол для настройки констрейнтов ViewController

---
+++ FakeNFT/Scenes /Common/Protocols/ViewSetupable.swift
@@ -0,0 +1,13 @@
+//
+//  ViewSetupable.swift
+//  FakeNFT
+//
+//  Created by Дмитрий on 10.09.2024.
+//
+
+import Foundation
+
+protocol ViewSetupable {
+    func addSubviews()
+    func addConstraints()
+}
