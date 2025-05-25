62860ce511a88819acae047d5110c64eed937430
Maxoscaro <svgerasimov92@yandex.ru>
2025-05-25 17:30:20 +0300

    Подготовка проекта под разработку на SwiftUI Удаление лишних файлов Добавление цветов и шрифтов

---
+++ FakeNFT/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FakeNFT
+//
+//  Created by Max on 24.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+     Text("Hello")
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
---
+++ FakeNFT/FakeNftApp.swift
@@ -0,0 +1,18 @@
+//
+//  FakeNftApp.swift
+//  FakeNFT
+//
+//  Created by Max on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FakeNftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
