c23481db04b26d1c2a09d96a2b173649e5521309
Maxoscaro <svgerasimov92@yandex.ru>
2025-05-29 18:06:25 +0300

    Поправил работу таббара и внес модель

--- FakeNFT/Scenes /ContentView.swift
+++ FakeNFT/Scenes /ContentView.swift
@@ -9,24 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var isTabBarHidden = false
     @State private var selectedTab = Tab.profile
+    @StateObject private var cartViewModel = CartViewViewModel()
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            NavigationStack {
-                TabView(selection: $selectedTab) {
-                    Text("Profile")
-                        .tag(Tab.profile)
-                    
-                    Text("Catalog")
-                        .tag(Tab.catalog)
-                    
-                    Text("Cart")
-                        .tag(Tab.cart)
-                    
-                    Text("Statistics")
-                        .tag(Tab.statistics)
-                }
+            TabView(selection: $selectedTab) {
+                Text("Profile")
+                    .tag(Tab.profile)
+                
+                Text("Catalog")
+                    .tag(Tab.catalog)
+                
+                Text("Cart")
+                    .tag(Tab.catalog)
+                
+                Text("Statistics")
+                    .tag(Tab.statistics)
+            }
+            if !isTabBarHidden {
                 TabBarView(selectedTab: $selectedTab)
             }
         }
