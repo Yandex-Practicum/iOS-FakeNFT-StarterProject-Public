081ac789a8caa69c3c08f8bf2863f5c6306aa4f3
Maxoscaro <svgerasimov92@yandex.ru>
2025-06-16 15:42:59 +0300

    Обработка состояний

--- FakeNFT/Scenes /Cart/ChoosePaymentView/ChoosePaymentView.swift
+++ FakeNFT/Scenes /Cart/ChoosePaymentView/ChoosePaymentView.swift
@@ -10,42 +10,49 @@ import SwiftUI
 struct ChoosePaymentView: View {
     
     @Binding var isTabBarHidden: Bool
+    @Binding var selectedTab: Tab
+    @Binding var showPayment: Bool
     @State private var showSuccessPayment = false
+    @State private var showPaymentAlert = false
     @StateObject private var viewModel = CurrencyViewModel()
     private let columns = Array(repeating: GridItem(.flexible(), spacing: 7), count: 2)
     
     var body: some View {
         Group {
-            switch viewModel.state {
-                
-            case.loading:
+            if viewModel.isProcessingPayment {
                 LoadingView()
-                
-            case.empty:
-                Text("Валюты не найдены")
-                    .font(.bold17)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                
-            case.loaded(let currencies):
-                VStack {
-                    ScrollView {
-                        LazyVGrid(columns: columns,  spacing: 7) {
-                            ForEach(currencies) { currency in
-                                CurrencyCellView(currency: currency, viewModel: viewModel)
+            } else {
+                switch viewModel.state {
+                    
+                case.loading:
+                    LoadingView()
+                    
+                case.empty:
+                    Text("Валюты не найдены")
+                        .font(.bold17)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                case.loaded(let currencies):
+                    VStack {
+                        ScrollView {
+                            LazyVGrid(columns: columns,  spacing: 7) {
+                                ForEach(currencies) { currency in
+                                    CurrencyCellView(currency: currency, viewModel: viewModel)
+                                }
+                            }
+                        }
+                        .padding()
+                        
+                        PaymentBottomPanel(viewModel: viewModel) {
+                            Task {
+                                await viewModel.processPayment()
                             }
                         }
                     }
-                    .padding()
                     
-                    PaymentBottomPanel(viewModel: viewModel, onPaymentTap: {
-                        showSuccessPayment = true
-                    }
-                    )
+                case .error(let errorMessage):
+                    Text("\(errorMessage)")
                 }
-                
-            case .error(let errorMessage):
-                Text("\(errorMessage)")
-                
             }
         }
         .modifier(NavigationBarStyle(
@@ -60,14 +67,32 @@ struct ChoosePaymentView: View {
         .task {
             await viewModel.loadCurrencies()
         }
+        .onChange(of: viewModel.paymentState) { state in
+            switch state {
+            case .success:
+                showSuccessPayment = true
+            case .failure:
+                showPaymentAlert = true
+            default:
+                break
+            }
+        }
+        .alert("Не удалось произвести оплату", isPresented: $showPaymentAlert) {
+            Button("Повторить") {
+                Task { await viewModel.processPayment() }
+            }
+            Button("Отмена", role: .cancel) {
+                viewModel.resetPayment()
+            }
+        }
         .navigationDestination(isPresented: $showSuccessPayment) {
-            SuccessPaymentView()
+            SuccessPaymentView( selectedTab: $selectedTab,
+                                isTabBarHidden: $isTabBarHidden, showPayment: $showPayment)
         }
     }
-    
 }
 
 #Preview {
-    ChoosePaymentView(isTabBarHidden: .constant(true))
+    ChoosePaymentView(isTabBarHidden: .constant(true), selectedTab: .constant(.cart), showPayment: .constant(true))
         .environmentObject(CartViewViewModel())
 }
