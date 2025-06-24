af1190aabf28cb07ccf820a07a33e9df671b4721
Vegicode <vadimegiazarov@mail.ru>
2025-06-24 10:50:02 +0300

    Revert "Добавил mvvm"
    This reverts commit 9ad1b1efda67ecfbea0638a2dc02f0703f7bf7cf.

--- FakeNFT/ProfileViewModel.swift
+++ FakeNFT/ProfileViewModel.swift
@@ -6,28 +6,15 @@
 //
 
 import Foundation
-import SwiftUI
+
 @MainActor
 
 final class ProfileViewModel: ObservableObject {
     
-    @Published var profile: ProfileModel
-    
-    init(profile: ProfileModel, name: String, description: String, link: String, imageData: Data? = nil) {
-        self.profile = profile
-        self.name = name
-        self.description = description
-        self.link = link
-        self.imageData = imageData
+    @Published var user: User
+    init(user: User) {
+        self.user = user
     }
-    @AppStorage("name") var name: String = "Joaquin Phoenix"
-    
-    @AppStorage("description") var description: String = "Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT,  и еще больше — на моём сайте. Открыт к коллаборациям."
-    
-    @AppStorage("link") var link: String = "link"
-    
-    @AppStorage("savedImage") var imageData: Data?
-   
     
 }
 
--- FakeNFT/Scenes /ContentView.swift
+++ FakeNFT/Scenes /ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     var body: some View {
         ZStack(alignment: .bottom) {
             TabView(selection: $selectedTab) {
-                ProfileView(viewModel: ProfileViewModel.init(profile: ProfileModel.init(name: "", description: "", link: ""), name: "", description: "", link: ""))
+                ProfileView()
                     .tag(Tab.profile)
                 
                 Text("Catalog")
