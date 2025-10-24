7d69134876cd1cdf2b7c8d7e2b226a65b178128b
NatAn <andrei.v.0204@gmail.com>
2025-10-24 18:45:25 +0300

    Перенес цвета из Figma в проект

--- FakeNFT/DesignSystem/Colors.swift
+++ FakeNFT/DesignSystem/Colors.swift
@@ -25,43 +25,42 @@ extension UIColor {
         )
     }
 
-    // Ниже приведены примеры цветов, настоящие цвета надо взять из фигмы
+    // MARK: - Theme Colors (Day/Night)
+        
+        static let black = UIColor { traits in
+            return traits.userInterfaceStyle == .dark
+            ? UIColor(hexString: "FFFFFF") // White [night]
+            : UIColor(hexString: "1A1B22") // Black [day]
+        }
+        
+        static let white = UIColor { traits in
+            return traits.userInterfaceStyle == .dark
+            ? UIColor(hexString: "1A1B22") // White [night]
+            : UIColor(hexString: "FFFFFF") // White [day]
+        }
+        
+        static let lightGray = UIColor { traits in
+            return traits.userInterfaceStyle == .dark
+            ? UIColor(hexString: "2C2C2E") // Light grey [night]
+            : UIColor(hexString: "F7F7F8") // Light grey [day]
+        }
 
-    // Primary Colors
-    static let primary = UIColor(red: 0 / 255, green: 122 / 255, blue: 255 / 255, alpha: 1.0)
+        // MARK: - Universal Colors
+        
+        static let greyUniversal = UIColor(hexString: "625C5C")
+        static let redUniversal = UIColor(hexString: "F56B6C")
+        static let greenUniversal = UIColor(hexString: "1C9F00")
+        static let blueUniversal = UIColor(hexString: "0A84FF")
+        static let yellowUniversal = UIColor(hexString: "FEEF0D")
+        
+        // Background Universal with 50% opacity
+        static let backgroundUniversal: UIColor = {
+            return UIColor(hexString: "1A1B22").withAlphaComponent(0.5)
+        }()
 
-    // Secondary Colors
-    static let secondary = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1.0)
-
-    // Background Colors
-    static let background = UIColor.white
-
-    // Text Colors
-    static let textPrimary = UIColor.black
-    static let textSecondary = UIColor.gray
-    static let textOnPrimary = UIColor.white
-    static let textOnSecondary = UIColor.black
-
-    private static let yaBlackLight = UIColor(hexString: "1A1B22")
-    private static let yaBlackDark = UIColor.white
-    private static let yaLightGrayLight = UIColor(hexString: "#F7F7F8")
-    private static let yaLightGrayDark = UIColor(hexString: "#2C2C2E")
-
-    static let segmentActive = UIColor { traits in
-        return traits.userInterfaceStyle == .dark
-        ? .yaBlackDark
-        : .yaBlackLight
+        // MARK: - Component Colors (для конкретных компонентов UI)
+        
+        static let segmentActive = black
+        static let segmentInactive = lightGray
+        static let closeButton = black
     }
-
-    static let segmentInactive = UIColor { traits in
-        return traits.userInterfaceStyle == .dark
-        ? .yaLightGrayDark
-        : .yaLightGrayLight
-    }
-
-    static let closeButton = UIColor { traits in
-        return traits.userInterfaceStyle == .dark
-        ? .yaBlackDark
-        : .yaBlackLight
-    }
-}
