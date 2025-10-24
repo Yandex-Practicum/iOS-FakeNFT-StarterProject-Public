0f7a67d79e986c78ae54a5a4aaade6006a87716f
NatAn <andrei.v.0204@gmail.com>
2025-10-24 16:15:15 +0300

    Расширил существующую NFT модель

--- FakeNFT/Models/Network/Nft.swift
+++ FakeNFT/Models/Network/Nft.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 struct Nft: Decodable {
-    let id: String
-    let images: [URL]
+    let createdAt: String
+        let name: String
+        let images: [URL]
+        let rating: Int
+        let description: String
+        let price: Float
+        let author: String
+        let id: String
 }
